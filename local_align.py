fd5df90bf5203ea08c067591d5e93d679754c805
benchugg <ben.ih.chugg@gmail.com>
2017-02-23 21:59:55 -0800

    implement k-optimal local alignment algorithm. More rigorous testing must still be done.

--- local_align.py
+++ local_align.py
@@ -1,181 +1,229 @@
 import numpy as np
 import sys
-from params import blosum, score_index, seq, seq_name, d, e
+from params import score_matrix, alphabet, d, e
+from Queue import Queue
 
-# GLOBAL VARS
-min_best_score = -1*sys.maxint
+""" 
+Global Vars
+"""
+# minimum score
+ms = -1*sys.maxint
+# score matrix
+sm = score_matrix
 
-		 
-# Parse each protein in file and compute their score
-# 	File: in fasta format			 
-# 	maximum (optional): maximum number of proteins to compare
-def parseAndComputeScore(file, maximum=sys.maxint):
-	num_proteins = 0
-	# Keep track of best matches: best_matches[i] = [name, index, score]
-	best_matches = [[[] for _ in range(7)] for _ in range(3)]
-	for i in range(0,3):
-		best_matches[i][2] = -1*sys.maxint
-	print best_matches
-	for line in file:
-		if line[0] == '>':
-			if num_proteins != 0:
-				align_details = computeScoreAndAlignment(amino_acids)
-				updateBestMatches(best_matches, align_details, name, index)
-			num_proteins += 1
-			if num_proteins >= maximum+1:
-				break
-			amino_acids = ''
-			# grab relevant protein info
-			index = num_proteins-1
-			name = line.split("|")[2].split(" ")[0]
-		else: 
-			amino_acids += line.strip('\n')
-
-	updateBestMatches(best_matches, computeScoreAndAlignment(amino_acids), name, index)
-	printMatches(best_matches)
-
+class localAlign:
+	"""
+	A class to solve local Alignment. Computes k-optimal disjoint
+	local alignments 
+	"""
 	
+	def __init__(self, x, y):
+		"""
+		x with length n, y with legth m are two sequences to compare
+		"""
+		self.x = x
+		self.y = y
+		self.n = len(x)+1
+		self.m = len(y)+1
 
-# Determines if current protein is among the top 3 matches so far
-#	best_matches is array of top 3 matches so far
-#	score, name and index are of current protein
-	# alignment = [score, X, Y, X_index, Y_index, length]	
-def updateBestMatches(best_matches, alignment, name, index):
-	min_index = np.argmin([best_matches[0][2], best_matches[1][2], best_matches[2][2]])
-	if best_matches[min_index][2] < alignment[0]:
-		best_matches[min_index][0] = name
-		best_matches[min_index][1] = index
-		best_matches[min_index][2] = alignment[0]
-		best_matches[min_index][3] = alignment[3]
-		best_matches[min_index][4] = alignment[4]
-		best_matches[min_index][5] = alignment[1]
-		best_matches[min_index][6] = alignment[2]
-	# Update max score
-	min_best_score = min(best_matches[0][2], best_matches[1][2], best_matches[2][2])	
-	print best_matches[0][0:3],
-	print best_matches[1][0:3],
-	print best_matches[2][0:3] 
 
-# Print names, scores, indices and alignments of top scores
-def printMatches(best_matches):
-	for i in range(3):
-		print "Index=", best_matches[i][1], 
-		print "Name=", best_matches[i][0],
-		print "Score=", best_matches[i][2]
-		print "START POS in", best_matches[i][0], ":", best_matches[i][3],
-		print "START POS in", seq_name, ": ", best_matches[i][4],
-		print "LENGTH: ", max(len(best_matches[i][5]), len(best_matches[i][6]))
-		print best_matches[i][5]
-		print best_matches[i][6]
+	def initializeMatrices(self,n,m):
+		""" 
+		Initialize matrices and pointer matrices for use in recurrence
+		"""
+		self.M, self.GX, self.GY = np.zeros((m,n)), np.zeros((m,n)), np.zeros((m,n))
+		self.Mp = [ [ [] for _ in range(n) ] for _ in range(m) ]
+		self.GXp = [ [ [] for _ in range(n) ] for _ in range(m) ]
+		self.GYp = [ [ [] for _ in range(n) ] for _ in range(m) ]
 
-# Compute alignment using Smith Waterman for local alignment
-#	aa is string of amino acids
-#   Return array of matching sc
-def computeScoreAndAlignment(aa):
-	n = len(seq) + 1
-	m = len(aa)+1
-	# Initialize matrices and pointer matrices
-	M, GX, GY = np.zeros((m,n)), np.zeros((m,n)), np.zeros((m,n))
-	Mpointer = [ [ [] for _ in range(n) ] for _ in range(m) ]
-	GXpointer = [ [ [] for _ in range(n) ] for _ in range(m) ]
-	GYpointer = [ [ [] for _ in range(n) ] for _ in range(m) ]
-
-	# Initialization
-	GX[0,0], GY[0,0] = -1*sys.maxint, -1*sys.maxint
-	Mpointer[0][0], GXpointer[0][0], GYpointer[0][0] = 'STOP', 'STOP', 'STOP'
-	for i in range(1,m):
-		GY[i,0] = -d
-		GX[i,0] = -d - (i-1)*e
-		GXpointer[i][0], GYpointer[i][0], Mpointer[i][0] = 'STOP', 'STOP', 'STOP'
-	for j in range(1,n):
-		GX[0,j] = -1*sys.maxint
-		GY[0,j] = -d 
-		GXpointer[0][j], GYpointer[0][j], Mpointer[0][j] = 'STOP', 'STOP', 'STOP'
-	
-	# Compute recurrence
-	for i in range(1,m):
-		b = findAAIndex(i-1,aa)
+		# Base Cases
+		self.GX[0,0], self.GY[0,0] = ms, ms
+		self.Mp[0][0], self.GXp[0][0], self.GYp[0][0] = 'STOP', 'STOP', 'STOP'
+		for i in range(1,m):
+			self.GY[i,0] = ms
+			self.GX[i,0] = -d - (i-1)*e
+			self.GXp[i][0], self.GYp[i][0], self.Mp[i][0] = 'STOP', 'STOP', 'STOP'
 		for j in range(1,n):
-			a = findAAIndex(j-1,seq)
-			# Choices in recurrence
-			M_choice = np.append(blosum[a,b]+np.array([M[i-1,j-1], GX[i-1,j-1], GY[i-1,j-1]]),[0])
-			GX_choice = [ M[i-1,j]-d, GX[i-1,j]-e ] 
-			GY_choice = [ M[i,j-1]-d, GY[i,j-1]-e ] 
-			# Max arguments
-			M_am = np.argmax(M_choice)
-			GX_am = np.argmax(GX_choice)
-			GY_am = np.argmax(GY_choice)
-			# Compute new values and update pointer matrices
-			M[i,j] = max(M_choice)
-			if M[i,j] == 0:
-				Mpointer[i][j] = 'STOP'
+			self.GX[0,j] = ms
+			self.GY[0,j] = -d - (j-1)*e
+			self.GXp[0][j], self.GYp[0][j], self.Mp[0][j] = 'STOP', 'STOP', 'STOP'
+		
+
+
+	def kOptimalAlignments(self, k):
+		""" 
+		Compute k Optimal disjoint local alignments between sequences x and y
+		""" 
+		self.k = k
+		self.alignments = ["" for _ in range(k)]
+		q = Queue()
+		q.put([self.x, self.y])
+		
+		while k > 0 and not q.empty():
+			subs = q.get()
+			align = self.computeAlignment(subs[0], subs[1])
+			self.alignments[self.k-k] = align
+			self.splitAlignments(align, subs[0], subs[1], q)
+			k -= 1
+
+		if k != 0:
+			opt = self.k-k
+			print "\nWarning: Unable to find",self.k,"optimal alignments"
+			print "Number of alignments found:",opt
+			print "Resetting Optimal k to",opt,
+			self.k = opt
+			print "... ... DONE\n"
+
+
+
+		
+
+	def splitAlignments(self, align, s1, s2, q):
+		"""
+		Split s1, s2 into subsequences before and after
+		sequence in align, place on queue q
+		align[0][0] is subsequence of s1, 
+		align[0][1] is subsequence of s2
+		"""
+		sub_s1 = align[0][0]
+		sub_s2 = align[0][1]
+
+		# Beginning sequences
+		start_s1 = s1[0:align[1]]
+		start_s2 = s2[0:align[2]]
+		if len(start_s1) > 0 and len(start_s2) > 0:
+			q.put([start_s1, start_s2])
+
+		# Ending sequences
+		end_s1 = s1[align[1]+len(sub_s1):len(s1)]
+		end_s2 = s1[align[2]+len(sub_s2):len(s2)]
+		if len(end_s1) > 0 and len(end_s2) > 0:
+			q.put([start_s1, start_s2])
+
+
+	
+	def computeAlignment(self, seq1, seq2):
+		"""
+		Fill out alignment matrices for local alignment between
+		sequences x and y. Return alignment, indices, and score
+		"""
+		# Initialize 
+		n = len(seq1) + 1
+		m = len(seq2) + 1
+		self.initializeMatrices(n, m)
+		for i in range(1, m):
+			b = self.findIndex(i-1,seq2)
+			for j in range(1, n):
+				a = self.findIndex(j-1,seq1)
+				# Choices in recurrence
+				M_choice = np.append(sm[a,b]+np.array([self.M[i-1,j-1], self.GX[i-1,j-1], self.GY[i-1,j-1]]),[0])
+				GX_choice = [ self.M[i-1,j]-d, self.GX[i-1,j]-e ] 
+				GY_choice = [ self.M[i,j-1]-d, self.GY[i,j-1]-e ] 
+				# Max arguments
+				M_am = np.argmax(M_choice)
+				GX_am = np.argmax(GX_choice)
+				GY_am = np.argmax(GY_choice)
+				# Compute new values and update pointer matrices
+				self.M[i,j] = max(M_choice)
+				if self.M[i,j] == 0:
+					self.Mp[i][j] = 'STOP'
+				else:
+					self.Mp[i][j] = 'M' if M_am == 0 else ('GX' if M_am == 1 else 'GY')
+				self.GX[i,j] = max(GX_choice)
+				self.GXp[i][j] = 'M' if GX_am == 0 else 'GX'
+				self.GY[i,j] = max(GY_choice)
+				self.GYp[i][j] = 'M' if GY_am == 0 else 'GY'
+
+		return self.traceback()
+		
+
+	
+	# TODO:: Should we be replacing U with C? Does that matter in our case?
+	def findIndex(self, index, sequence):
+		""" 
+		Find index in alphabet of sequence[index]
+		"""
+		letter = sequence[index]
+		if letter == 'U':
+			letter = 'C'
+		return alphabet.index(letter)
+
+	
+	
+	def traceback(self):
+		"""
+		Traceback procedure for computing alignment
+		M is scoring matrix, [M,GX,GY]p are pointer matrices
+
+		Returns [align, i, j]
+		align[1] is subsequence of x
+		align[0] is subsequence of y
+		j is index in x where local alignment begins (x[i])
+		i is index of y where local alignments begins (y[j])
+		"""
+		align = ["", ""]
+		table = self.Mp
+		print self.M
+		score = np.max(self.M)
+		am = np.argmax(self.M)
+		print "ARGMAX M::"
+		j = am % self.M.shape[1]
+		i = am / self.M.shape[1]
+		print "i=", i, "j=", j
+		while True:
+			if table == self.Mp:
+				if table[i][j] == 'GX':
+					table = self.GXp
+				elif table[i][j] == 'GY':
+					table = self.GYp
+				align[0] = self.y[i-1] + align[0]
+				align[1] = self.x[j-1] + align[1]
+				i -= 1
+				j -= 1
+			elif table == self.GXp:
+				if table[i][j] == 'M':
+					table = self.Mp
+				align[0] = self.y[i-1] + align[0]
+				align[1] = "-" + align[1]
+				i -= 1
 			else:
-				Mpointer[i][j] = 'M' if M_am == 0 else ('GX' if M_am == 1 else 'GY')
-			GX[i,j] = max(GX_choice)
-			GXpointer[i][j] = 'M' if GX_am == 0 else 'GX'
-			GY[i,j] = max(GY_choice)
-			GYpointer[i][j] = 'M' if GY_am == 0 else 'GY'
-	
-	score = np.max(M)
-	if score > min_best_score:
-		align = traceback(M,Mpointer,GXpointer,GYpointer,aa)
-		return [score, align[0][0], align[0][1], align[1], align[2]]
-	else:
-		return [score, "", "", "", ""]
+				if table[i][j] == 'M':
+					table = self.Mp
+				align[0] = "-" + align[0]
+				align[1] = self.x[j-1] + align[1]
+				j -= 1
+			if table[i][j] == 'STOP':
+				break
 
-# Traceback procedure for computing an alignment
-#	M is score matrix, Mp, GXp, GYp are respective pointer matrices
-#   Return 2d-array align, align[0] is subsequence of x, 
-#	align[1] is subsequence of y
-def traceback(M,Mp,GXp,GYp,aa):
-	align = ["", ""]
-	table = Mp
-	am = np.argmax(M)
-	j = am % M.shape[1]
-	i = am / M.shape[1]
-	while True:
-		if table == Mp:
-			if table[i][j] == 'GX':
-				table = GXp
-			elif table[i][j] == 'GY':
-				table = GYp
-			align[0] = aa[i-1] + align[0]
-			align[1] = seq[j-1] + align[1]
-			i -= 1
-			j -= 1
-		elif table == GXp:
-			if table[i][j] == 'M':
-				table = Mp
-			align[0] = aa[i-1] + align[0]
-			align[1] = "-" + align[1]
-			i -= 1
+		return [[align[1], align[0]], j, i, score]
+
+
+
+	def printAlignment(self, index=""):
+		""" 
+		Print Alignment. If index argument specified, print only 
+		that alignment. Otherwise print all. 
+		"""
+		if index == "":
+			for i in range(self.k):
+				self.printAlignmentHelper(i)
 		else:
-			if table[i][j] == 'M':
-				table = Mp
-			align[0] = "-" + align[0]
-			align[1] = seq[j-1] + align[1]
-			j -= 1
-		if table[i][j] == 'STOP':
-			break
+			index = int(index)
+			self.printAlignmentHelper(index)
 
-	return [align, i+1, j+1]
 
-	
-# Find amino acid corresponding to index in given sequence
-# If letter is U, switch it for C
-def findAAIndex(index, sequence):
-	letter = sequence[index]
-	if letter == 'U':
-		letter = 'C'
-	return score_index.index(letter)
-	
 
-def main(argv):
-	if len(argv) < 2:
-		file = open('uniprot.fasta', 'r')
-	else: 
-		file = open(argv[1], 'r')
+	def printAlignmentHelper(self, i):
+		"""
+		Print single alignment
+		"""
+		print "SCORE: ", self.alignments[i][3]
+		print "Index of x:", self.alignments[i][1], 
+		print "Index of y:", self.alignments[i][2]
+		print "Alignment: "
+		print "x: ",
+		print self.alignments[i][0][0][0:60]
+		print "y: ",
+		print self.alignments[i][0][1][0:60]
 
-	parseAndComputeScore(file)
 
-main(sys.argv)
