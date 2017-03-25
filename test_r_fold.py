6ae705362ab05966cc7bffcba65fd7dc2f92f89e
Coulter Beeson <coulter.beeson@gmail.com>
2017-03-25 16:36:01 -0700

    Switched to iteration  Please enter the commit message for your changes. Lines starting

--- test_r_fold.py
+++ test_r_fold.py
@@ -1,15 +1,75 @@
 from r_fold import RNA_Fold as RF
+from random import random
+from math import *
+import sys
 
-seq = "GGGAAAUCC"
 
 rf = RF()
 
+
+def gen_rand_seq(n):
+
+	alph = ['A','C','U','G']
+	seq = ""
+	for i in xrange(n):
+		r = int(ceil(4*random()))
+		seq += alph[r-1]
+	return seq
+
+
+#-------------------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------------------
+
+#sys.setrecursionlimit(1200)
+
+#test 1
+#Expect 3
+seq = "GGGAAAUCC"
 print("case1: " + str(rf.fold(seq)))
 
-seq = "CGUCACAGUAGAAGCCUAUAUGUGAGGCUACUCGGC"
-
+#test2
+#Expect 10 
+seq = "GGGUUUGGGCCCUUUGGUGGCCCCUUUCCC"
 print("case2: " + str(rf.fold(seq)))
-rf.print_F()
+
+
+#test3
+#Expect 16
+seq = "CGUCACAGUAGAAGCCUAUAUGUGAGGCUACUCGGC"
+print("case3: " + str(rf.fold(seq)))
+
+
+#test4
+#Expect ?
+seq = "ACACGACCUCAUAUAAUCUUGGGAAUAUGGCCCAUAAGUUUCUACCCGGCAACCGUAAAUUGCCGGACUAUGCAGGGAAGUGA"
+print("case4: " + str(rf.fold(seq)))
+
+#test5
+#Expect random 
+seq = gen_rand_seq(200)
+print(seq)
+print("case5: " + str(rf.fold(seq)))
+
+#test6
+#Expect random 
+seq = gen_rand_seq(200)
+print(seq)
+print("case6: " + str(rf.fold(seq)))
+
+#test7
+#Expect random 
+seq = gen_rand_seq(200)
+print(seq)
+print("case7: " + str(rf.fold(seq)))
+
+#test8
+#Expect random 
+seq = gen_rand_seq(200)
+print(seq)
+print("case8: " + str(rf.fold(seq)))
+
+
 
 
 
