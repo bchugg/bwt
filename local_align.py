01fcf1d147f6fa71bf407303aa3f284f42730893
benchugg <ben.ih.chugg@gmail.com>
2017-03-26 16:01:24 -0700

    revamp local alignment; begin main rna processing

--- local_align.py
+++ local_align.py
@@ -1,5 +1,6 @@
 import numpy as np
 import sys
+from operator import itemgetter
 from params import score_matrix, alphabet, d, e
 from queue import Queue
 
@@ -38,32 +39,38 @@ class disjointAlignments:
 		Compute k Optimal disjoint local alignments between sequences x and y
 		""" 
 		self.k = k
-		self.alignments = ["" for _ in range(k)]
+		self.alignments = [[] for _ in range(k)]
+		
 		q = Queue()
 		q.put([self.x, self.y])
+		indices = Queue()
+		indices.put([0,0])
 		
-		print(k)
-		print(not q.empty())
+		
 		while k > 0 and not q.empty():
 			subs = q.get()
 			align = localAlign(subs[0], subs[1])
-			align.computeAlignment()
-			print("Computed an optimal alignment")
-			self.alignments[self.k-k] = align.align_info
-			self.splitAlignments(align.align_info, subs[0], subs[1], q)
-			k -= 1
+			inds = indices.get()
+			align.computeAlignment(inds[0], inds[1])
+			if  not align.emptyAlign():
+				self.alignments[self.k-k] = align.align_info
+				self.splitAlignments(align.align_info, subs[0], subs[1], q, indices, inds)
+				k -= 1
 
 		if k != 0:
 			opt = self.k-k
 			print("\nWarning: Unable to find",self.k,"optimal alignments")
 			print("Number of alignments found:",opt)
-			print("Resetting Optimal k to",opt, end='')
+			print("Resetting Optimal k to",opt, end=' ')
 			self.k = opt
 			print("... ... DONE\n")
 
+		#Sort by index of x
+		self.alignments = sorted(self.alignments,key=itemgetter(1))
 
 
-	def splitAlignments(self, align, s1, s2, q):
+
+	def splitAlignments(self, align, s1, s2, q, indices, inds):
 		"""
 		Split s1, s2 into subsequences before and after
 		sequence in align, place on queue q
@@ -72,32 +79,46 @@ class disjointAlignments:
 		"""
 		sub_s1 = align[0][0]
 		sub_s2 = align[0][1]
+		# print("Just computed alignment:")
+		# print(sub_s1)
+		# print(sub_s2)
+		# print("At indices:")
+		# print('Index of x', align[1], 'Index of y:', align[2])
 
-		# Beginning sequences
-		start_s1 = s1[0:align[1]]
-		start_s2 = s2[0:align[2]]
+		# # Beginning sequences
+		start_s1 = s1[0:s1.find(sub_s1)]
+		start_s2 = s2[0:s2.find(sub_s2)]
 		if len(start_s1) > 0 and len(start_s2) > 0:
 			q.put([start_s1, start_s2])
+			indices.put([inds[0], inds[1]])
+			# print("Putting sequences on Stack:")
+			# print(start_s1)
+			# print(start_s2)
+			# print('At indices:')
+			# print('Index of x', inds[0], 'Index of y:', inds[1])
 
 		# Ending sequences
-		end_s1 = s1[align[1]+len(sub_s1):len(s1)]
-		end_s2 = s1[align[2]+len(sub_s2):len(s2)]
+		end_s1 = s1[s1.find(sub_s1)+len(sub_s1):]
+		end_s2 = s2[s2.find(sub_s1)+len(sub_s2):]
 		if len(end_s1) > 0 and len(end_s2) > 0:
-			q.put([start_s1, start_s2])
+			q.put([end_s1, end_s2])
+			indices.put([inds[0]+len(sub_s1)+len(start_s1), inds[1]+len(sub_s2)+len(start_s2)])
+			# print("Putting sequences on Stack:")
+			# print(end_s1)
+			# print(end_s2)
+			# print('At indices:')
+			# print('Index of x', inds[0]+len(sub_s1)+len(start_s1), 'Index of y:', inds[1]+len(sub_s2)+len(start_s2))
 
 
 
-	def printAlignments(self, index=""):
+	def printAlignments(self):
 		""" 
 		Print Alignment. If index argument specified, print only 
 		that alignment. Otherwise print all. 
 		"""
-		if index == "":
-			for i in range(self.k):
+		for i in range(self.k):
 				localAlign.printAlignment(self.alignments[i])
-		else:
-			localAlign.printAlignment(self.alignments[int(index)])
-
+		
 
 
 
@@ -117,32 +138,9 @@ class localAlign:
 		self.y = y
 		self.n = len(x)+1
 		self.m = len(y)+1
-
-
-	def initializeMatrices(self,n,m):
-		""" 
-		Initialize matrices and pointer matrices for use in recurrence
-		"""
-		self.M, self.GX, self.GY = np.zeros((m,n)), np.zeros((m,n)), np.zeros((m,n))
-		self.Mp = [ [ [] for _ in range(n) ] for _ in range(m) ]
-		self.GXp = [ [ [] for _ in range(n) ] for _ in range(m) ]
-		self.GYp = [ [ [] for _ in range(n) ] for _ in range(m) ]
-
-		# Base Cases
-		self.GX[0,0], self.GY[0,0] = ms, ms
-		self.Mp[0][0], self.GXp[0][0], self.GYp[0][0] = 'STOP', 'STOP', 'STOP'
-		for i in range(1,m):
-			self.GY[i,0] = ms
-			self.GX[i,0] = -d - (i-1)*e
-			self.GXp[i][0], self.GYp[i][0], self.Mp[i][0] = 'STOP', 'STOP', 'STOP'
-		for j in range(1,n):
-			self.GX[0,j] = ms
-			self.GY[0,j] = -d - (j-1)*e
-			self.GXp[0][j], self.GYp[0][j], self.Mp[0][j] = 'STOP', 'STOP', 'STOP'
-		
-
 	
-	def computeAlignment(self):
+
+	def computeAlignment(self, xoff, yoff):
 		"""
 		Fill out alignment matrices for local alignment between
 		sequences x and y. Return alignment, indices, and score
@@ -150,34 +148,15 @@ class localAlign:
 		# Initialize 
 		n = len(self.x) + 1
 		m = len(self.y) + 1
-		self.initializeMatrices(n, m)
-
+		self.M = np.zeros((m,n))
 		for i in range(1, m):
 			b = self.findIndex(i-1,self.y)
 			for j in range(1, n):
 				a = self.findIndex(j-1,self.x)
-				# Choices in recurrence
-				M_choice = np.append(sm[a,b]+np.array([self.M[i-1,j-1], self.GX[i-1,j-1], self.GY[i-1,j-1]]),[0])
-				GX_choice = [ self.M[i-1,j]-d, self.GX[i-1,j]-e ] 
-				GY_choice = [ self.M[i,j-1]-d, self.GY[i,j-1]-e ] 
-				# Max arguments
-				M_am = np.argmax(M_choice)
-				GX_am = np.argmax(GX_choice)
-				GY_am = np.argmax(GY_choice)
-				# Compute new values and update pointer matrices
-				self.M[i,j] = max(M_choice)
-				if self.M[i,j] == 0:
-					self.Mp[i][j] = 'STOP'
-				else:
-					self.Mp[i][j] = 'M' if M_am == 0 else ('GX' if M_am == 1 else 'GY')
-				self.GX[i,j] = max(GX_choice)
-				self.GXp[i][j] = 'M' if GX_am == 0 else 'GX'
-				self.GY[i,j] = max(GY_choice)
-				self.GYp[i][j] = 'M' if GY_am == 0 else 'GY'
+				choice = [self.M[i-1,j-1]+sm[a,b],0]
+				self.M[i,j] = max(choice)
 
-		return self.traceback()
-		
-
+		self.traceback(xoff, yoff)
 
 	def findIndex(self, index, sequence):
 		""" 
@@ -190,49 +169,36 @@ class localAlign:
 
 	
 	
-	def traceback(self):
+	def traceback(self, xoff, yoff):
 		"""
 		Traceback procedure for computing alignment
-		M is scoring matrix, [M,GX,GY]p are pointer matrices
-
-		Returns [align, i, j]
-		align[1] is subsequence of x
-		align[0] is subsequence of y
+		
+		Returns [align, i, j, score]
+		align[0] is subsequence of x
+		align[1] is subsequence of y
 		j is index in x where local alignment begins (x[i])
 		i is index of y where local alignments begins (y[j])
 		"""
 		align = ["", ""]
-		table = self.Mp
 		score = np.max(self.M)
 		am = np.argmax(self.M)
 		j = am % self.M.shape[1]
 		i = int(am / self.M.shape[1])
-		while True:
-			if table == self.Mp:
-				if table[i][j] == 'GX':
-					table = self.GXp
-				elif table[i][j] == 'GY':
-					table = self.GYp
-				align[0] = self.y[i-1] + align[0]
-				align[1] = self.x[j-1] + align[1]
-				i -= 1
-				j -= 1
-			elif table == self.GXp:
-				if table[i][j] == 'M':
-					table = self.Mp
-				align[0] = self.y[i-1] + align[0]
-				align[1] = "-" + align[1]
-				i -= 1
-			else:
-				if table[i][j] == 'M':
-					table = self.Mp
-				align[0] = "-" + align[0]
-				align[1] = self.x[j-1] + align[1]
-				j -= 1
-			if table[i][j] == 'STOP':
-				break
+		while self.M[i,j] != 0:
+			align[0] = self.x[j-1] + align[0]
+			align[1] = self.y[i-1] + align[1]
+			self.M[i,j] = 0
+			i -= 1
+			j -= 1
+		self.align_info = [[align[0], align[1]], j+xoff,i+yoff, score]
 
-		self.align_info = [[align[1], align[0]], j, i, score]
+	def emptyAlign(self):
+		""" 
+		Return true if empty alignment
+		"""
+		a1 = self.align_info[0][0]
+		a2 = self.align_info[0][1]
+		return (len(a1) == 0 or len(a2) == 0)
 
 	
 	@staticmethod
@@ -241,13 +207,15 @@ class localAlign:
 		Print single alignment
 		"""
 		print("SCORE: ", alignment[3])
-		print("Index of x:", alignment[1], end='') 
-		print("Index of y:", alignment[2])
+		print('LENGTH: ', len(alignment[0][0]))
+		print("Index of x:", alignment[1]) 
+		print("Index of y:", alignment[2]) 
 		print("Alignment: ")
 		print("x: ", end='')
 		print(alignment[0][0][0:60])
 		print("y: ", end='')
 		print(alignment[0][1][0:60])
+		print('\n')
 
 
 
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # Dependencies
 import sys
+import numpy as np
 from local_align import disjointAlignments as DA
 from local_align import localAlign as LA
 from r_fold import RNA_Fold as RF
@@ -11,11 +12,7 @@ Main File for running BWT_RNA
 Written jointly by:
 Coulter Beeson, Ben Chugg, Kenny Drabble, Jeffrey Jeyachandren
 
-* Run Instructions *
 
-"""
-
-"""
 STEPS:
 
 1) Call k-Optimal Local Alignment with specific k
@@ -30,8 +27,60 @@ together.
 
 5) Output in informative manner. 
 
-
 """
+
+seq = "UAUAUAUAUAGCAUGAGUGUCGCGCGCGC"
+k = 2
+sep = '*'*60 + '\n'
+
+# Find k optimal alignments between seq and itself
+rev = seq[::-1]
+align = DA(seq,rev)
+align.kAlignments(k)
+
+print(sep)
+print('Step 1: K-OPTIMAL LOCAL ALIGNMENTS\n')
+align.printAlignments()
+print(sep)
+
+# Pass leftover sequences into RNA folding. 
+print('Step 2: PASS UNMATCHED SEQUENCES TO RNA FOLDER\n')
+
+leftovers = [[]]
+newseq = seq
+cumul_ind = 0
+for i in range(align.k):
+	cut = align.alignments[i][0][0]
+	if len(cut) > 0:
+		ind = newseq.find(cut)
+		leftovers += [[newseq[:ind], cumul_ind]]
+		cumul_ind += ind + len(cut)
+		newseq = newseq[ind+len(cut):]
+if len(newseq) != 0:
+	leftovers = np.append(leftovers, [])
+
+if np.size(leftovers) >0:
+	print('Leftover Sequences to be passed into RNA Folding:')
+	for i in range(1,np.size(leftovers)):
+		s = leftovers[i][0]
+		l = leftovers[i][1]
+		print('Sequence:')
+		print(s)
+		print('Index: ', l, '-', l+len(s))
+else:
+	print('No unmatched regions found.')
+
+
+# Run RNA folding on Leftovers 
+rf = RF()
+for i in range(1,np.size(leftovers)):
+	if len(leftovers[i][0]) != 0:
+		rf.fold(leftovers[i][0])
+		rf.print_F()
+
+
+
+
 
 
 	
